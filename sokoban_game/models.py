19c7d70b8c46a6cb0913715e1d45dc5754be862c
DM7935-GH <davidmuoneke45@gmail.com>
2025-03-19 23:01:39 +0000

    Hopefully this merges the reverted changes from the merge-features branch with the new ones Jonathan added. No guarantees that it works.

--- sokoban_game/models.py
+++ sokoban_game/models.py
@@ -3,13 +3,9 @@ from core_app.models import UserAccount
 import json
 
 class sokoban_level(models.Model):
-    number = models.IntegerField(unique=True)
     map_data = models.TextField()
     box_positions = models.TextField()
     person_position = models.TextField()
-
-    def __str__(self):
-        return f"Level {self.number}"
 
     def get_map(self):
         return json.loads(self.map_data)
@@ -23,14 +19,8 @@ class sokoban_level(models.Model):
 
 # IDK if we're keeping this
 class sokoban_results(models.Model):
-    user_id = models.ForeignKey(
-        UserAccount,
-        on_delete=models.CASCADE,
-    )
-    level_id = models.ForeignKey(
-        sokoban_level,
-        on_delete=models.CASCADE,
-    )
+    user_id = models.ForeignKey(UserAccount, on_delete=models.CASCADE,)
+    level_id = models.ForeignKey(sokoban_level, on_delete=models.CASCADE,)
     steps = models.IntegerField(default=0)
 
     def __str__(self):
